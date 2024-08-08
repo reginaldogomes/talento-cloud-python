d8a0cf6d62fa0258de7f9d5577cac5ad3485d98c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-08-08 17:12:24 -0300

    feat: calculadora algoritmo

---
+++ calculadora.py
@@ -0,0 +1,53 @@
+def calcular(num1: int, num2: int, operacao: int) -> int:
+    if operacao == 1:
+        return somar(num1, num2)
+    elif operacao == 2:
+        return subtrair(num1, num2)
+    elif operacao == 3:
+        return multiplicar(num1, num2)
+    elif operacao == 4:
+        return dividir(num1, num2)
+    else:
+        return operacao_invalida()
+
+def somar(a: int, b: int) -> int:
+    return a + b
+
+def subtrair(a: int, b: int) -> int:
+    return a - b
+
+def multiplicar(a: int, b: int) -> int:
+    return a * b
+
+def dividir(a: int, b: int) -> int:
+    if b == 0:
+        print("Erro: Divisão por zero não permitida.")
+        return 0
+    return a // b  # Divisão inteira
+
+def operacao_invalida() -> int:
+    print("Erro: Operação inválida.")
+    return 0
+
+def main():
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
+    operacao = int(input("Digite a operação (1: Soma, 2: Subtração, 3: Multiplicação, 4: Divisão): "))
+    
+    resultado = calcular(num1, num2, operacao)
+
+    if operacao == 1:
+      operacao_str = "Soma"
+    elif operacao == 2:
+      operacao_str = "Subtração"
+    elif operacao == 3:
+      operacao_str = "Multiplicação"
+    elif operacao == 4:
+      operacao_str = "Divisão"
+    else:
+      operacao_str = "Operação Inválida"
+
+    print(f"Resultado da {operacao_str}: {resultado}")
+
+if __name__ == "__main__":
+    main()
